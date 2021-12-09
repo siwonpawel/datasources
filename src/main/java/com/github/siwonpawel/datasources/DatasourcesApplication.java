24291f1e8e3c836b7a42d4e31ec595202bce0ddc
Pawel Siwon <siwonpawel@gmail.com>
2021-12-09 19:40:37 +0100

    Init commit

---
+++ src/main/java/com/github/siwonpawel/datasources/DatasourcesApplication.java
@@ -0,0 +1,13 @@
+package com.github.siwonpawel.datasources;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatasourcesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatasourcesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/siwonpawel/datasources/DatasourcesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.siwonpawel.datasources;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatasourcesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
